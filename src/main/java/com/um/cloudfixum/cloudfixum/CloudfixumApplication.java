39c71fb0cdedb89cdfe137830c5e2572d7f577e7
sebaf96 <sefernandez96@gmail.com>
2020-06-10 19:24:45 -0300

    Initial commit, Hello World project

---
+++ src/main/java/com/um/cloudfixum/cloudfixum/CloudfixumApplication.java
@@ -0,0 +1,13 @@
+package com.um.cloudfixum.cloudfixum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudfixumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudfixumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/um/cloudfixum/cloudfixum/CloudfixumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.um.cloudfixum.cloudfixum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudfixumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
