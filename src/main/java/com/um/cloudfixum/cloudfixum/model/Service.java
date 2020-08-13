6c7cc043bd77915264ad07c3bcfc5bd0ac235cc0
nico-olivares22 <nicolaso2604@gmail.com>
2020-08-13 09:59:55 -0300

    changes in Service Class and pom

--- src/main/java/com/um/cloudfixum/cloudfixum/model/Service.java
+++ src/main/java/com/um/cloudfixum/cloudfixum/model/Service.java
@@ -1,20 +1,42 @@
 package com.um.cloudfixum.cloudfixum.model;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
+import java.time.LocalDate;
+import com.um.cloudfixum.cloudfixum.common.Identificable;
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
-public class Service implements Serializable {
-    private static final long serialVersionUID = 3159888465448526457L;
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+public class Service implements Serializable, Identificable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id_service;
-    private String service_name;
-    private String image_service;
+    @NotEmpty
+    private Long id;
+    @NotEmpty
+    @Size(min = 5, max = 50)
+    private String title;
+    @NotEmpty
     private String category;
+    @NotEmpty
     private Long base_price;
+    @NotEmpty
+    @Max(256)
     private String description;
+    @NotEmpty
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate date;
 }
