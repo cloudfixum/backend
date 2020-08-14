73f978d58e2e170367a9616ddb2bcfb87d286d17
nico-olivares22 <nicolaso2604@gmail.com>
2020-08-14 15:51:57 -0300

    create Enum Category

---
+++ src/main/java/com/um/cloudfixum/cloudfixum/model/Category.java
@@ -0,0 +1,36 @@
+package com.um.cloudfixum.cloudfixum.model;
+import lombok.Getter;
+import lombok.ToString;
+@Getter
+@ToString
+public enum Category {
+    BARBER("Barber", ""),
+    ELECT_TECH("Electrical Technician", ""),
+    GAS_OPERATOR("Gas Operator", ""),
+    PLUMBER("Plumber", ""),
+    METALLURGICAL("Metallurgical", ""),
+    BRICKLAYER("Bricklayer", ""),
+    GARDENER("Gardener",""),
+    CAREGIVER("Caregiver",""),
+    MAID("Maid", ""),
+    REPAIR_TECH("Repair Technician", ""),
+    MECHANIC("Mechanic",""),
+    TECH_SERVICE("Technician Service", ""),
+    PRIVATE_TUTORING("Private Tutoring ", ""),
+    PERSONAL_TRAINER("Personal Trainer", ""),
+    CARPENTER("Carpenter", ""),
+    GLAZIER("Glazier", ""),
+    BLACKSMITH("Blacksmith", ""),
+    COBBLER("Cobbler", ""),
+    DRESSMAKER("Dressmaker", ""),
+    AIRCON_TECH("Air Conditioning Technician", ""),
+    PAINTER("Painter", "");
+
+    private String name;
+    private String image_url;
+
+    Category (String name, String image_url) {
+        this.name = name;
+        this. image_url = image_url;
+    }
+}
