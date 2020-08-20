e64f4f8c7d351e62770f39eeb15125ec15c10ea7
sebaf96 <sefernandez96@gmail.com>
2020-08-20 20:06:56 -0300

    Added custom response to 404 error, home page now redirects to API Docs. API Docs now only tracks controllers under /api prefix

--- src/main/java/com/um/cloudfixum/cloudfixum/controller/HomeController.java
+++ src/main/java/com/um/cloudfixum/cloudfixum/controller/HomeController.java
@@ -3,12 +3,13 @@ package com.um.cloudfixum.cloudfixum.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 public class HomeController {
 
-    @GetMapping("")
-    public String helloWorld() {
-        return "Hello world!";
+    @GetMapping
+    public RedirectView home() {
+        return new RedirectView("/swagger-ui.html");
     }
 }
