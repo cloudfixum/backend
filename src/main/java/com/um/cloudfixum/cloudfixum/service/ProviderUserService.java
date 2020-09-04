ad84d573dccf068c9d9638419d088d525c6ffbae
Santiago RIera <rierasanti@hotmail.com>
2020-09-04 19:51:57 -0300

    Adding method to login with DNI

--- src/main/java/com/um/cloudfixum/cloudfixum/service/ProviderUserService.java
+++ src/main/java/com/um/cloudfixum/cloudfixum/service/ProviderUserService.java
@@ -28,6 +28,13 @@ public class ProviderUserService extends GenericServiceImpl<ProviderUser> {
 
     }
 
+    public ResponseEntity<ProviderUser> findUserByDni(String dni){
+        if (!providerUserRepository.findByDni(dni).isPresent()) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        ProviderUser providerUser = providerUserRepository.findByDni(dni).get();
+        return new ResponseEntity<>(providerUser,HttpStatus.OK);
+    }
+
+
     @Override
     public JpaRepository<ProviderUser, Long> getRepository() {
         return providerUserRepository;
