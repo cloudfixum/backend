185c413b521faef4eb1879fb5a9c5ca868460a00
sebaf96 <sefernandez96@gmail.com>
2020-09-08 21:56:41 -0300

    Giving CORS origin to HomeController

--- src/main/java/com/um/cloudfixum/cloudfixum/controller/HomeController.java
+++ src/main/java/com/um/cloudfixum/cloudfixum/controller/HomeController.java
@@ -3,16 +3,16 @@ package com.um.cloudfixum.cloudfixum.controller;
 
 import com.um.cloudfixum.cloudfixum.model.ProviderUser;
 import com.um.cloudfixum.cloudfixum.service.ProviderUserService;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
-import javax.validation.Valid;
 
+
+@CrossOrigin
 @RestController
 public class HomeController {
     private final ProviderUserService providerUserService;
