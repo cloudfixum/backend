66164c48d397e0056addb8db2344c1c072dcce84
s.martinez <santiskatersan@gmail.com>
2020-10-01 15:05:35 -0300

    Basic controller created but class Budget not mapped with ServiceProvider

--- src/main/java/com/um/cloudfixum/cloudfixum/service/BudgetService.java
+++ src/main/java/com/um/cloudfixum/cloudfixum/service/BudgetService.java
@@ -4,7 +4,9 @@ import com.um.cloudfixum.cloudfixum.common.GenericServiceImpl;
 import com.um.cloudfixum.cloudfixum.model.Budget;
 import com.um.cloudfixum.cloudfixum.repository.BudgetRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BudgetService extends GenericServiceImpl<Budget> {
 
     private final BudgetRepository budgetRepository;
