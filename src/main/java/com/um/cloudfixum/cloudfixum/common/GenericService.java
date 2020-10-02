9f57c38855c217162f389f4ca803f9283e08d661
Santiago RIera <rierasanti@hotmail.com>
2020-10-02 19:39:41 -0300

    Now sort by ID, original pagination method is eliminated

--- src/main/java/com/um/cloudfixum/cloudfixum/common/GenericService.java
+++ src/main/java/com/um/cloudfixum/cloudfixum/common/GenericService.java
@@ -20,7 +20,5 @@ public interface GenericService<T extends Serializable> {
 
     ResponseEntity<HttpStatus> delete(Long id);
 
-    ResponseEntity<List<T>> findByPage(int page, int size, HttpServletRequest request);
-
-    ResponseEntity<List<T>> findByPageFiltered(int page, int size, HttpServletRequest request,List<T> filteredList);
+    ResponseEntity<List<T>> findByPage(int page, int size, HttpServletRequest request,List<T> query_list);
 }
