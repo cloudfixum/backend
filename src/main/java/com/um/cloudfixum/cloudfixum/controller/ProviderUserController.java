88e9b6db1f67df93c1fffed82bb70f1afa1e60c5
s.martinez <santiskatersan@gmail.com>
2020-10-06 20:42:14 -0300

    First getbudgets from providerUser

--- src/main/java/com/um/cloudfixum/cloudfixum/controller/ProviderUserController.java
+++ src/main/java/com/um/cloudfixum/cloudfixum/controller/ProviderUserController.java
@@ -1,7 +1,9 @@
 package com.um.cloudfixum.cloudfixum.controller;
 
+import com.um.cloudfixum.cloudfixum.model.Budget;
 import com.um.cloudfixum.cloudfixum.model.MinorJob;
 import com.um.cloudfixum.cloudfixum.model.ProviderUser;
+import com.um.cloudfixum.cloudfixum.service.BudgetService;
 import com.um.cloudfixum.cloudfixum.service.ProviderUserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,9 +20,11 @@ import java.util.Optional;
 @RequestMapping("/api/user")
 public class ProviderUserController {
     private final ProviderUserService providerUserService;
+    private final BudgetService budgetService;
 
-    public ProviderUserController(ProviderUserService providerUserService) {
+    public ProviderUserController(ProviderUserService providerUserService, BudgetService budgetService) {
         this.providerUserService = providerUserService;
+        this.budgetService = budgetService;
     }
 
     @PostMapping
@@ -38,6 +43,25 @@ public class ProviderUserController {
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<List<MinorJob>> getJobsByUserID(@PathVariable Long id) {
         return providerUserService.getJobs(id);
+    }
+    @GetMapping("/budgets")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<List<Budget>> getBudgetsByServiceProvider(Authentication authentication){
+        List<Budget> budgets = budgetService.getRepository().findAll();
+        List<Budget>providerUserBudgets = new ArrayList<>();
+        if (authentication != null ) {
+            System.out.println(budgets);
+            for ( Budget budget : budgets) {
+                if (budget.getMinorJob().getServiceProvider().getEmail().equals(authentication.getName())){
+                    providerUserBudgets.add(budget);
+                }
+                System.out.println(providerUserBudgets);
+                return new  ResponseEntity<>(providerUserBudgets,HttpStatus.OK);
+            }
+        }
+        return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+
+
     }
 
     @DeleteMapping("/{id}")
