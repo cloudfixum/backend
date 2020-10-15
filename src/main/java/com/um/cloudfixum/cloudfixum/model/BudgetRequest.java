6d3e5a84293bccfb4443428ea6f2aee51e22abd2
YagoMaureira <y.maureira@alumno.um.edu.ar>
2020-10-15 17:37:46 -0300

    Add BudgetRequest class

---
+++ src/main/java/com/um/cloudfixum/cloudfixum/model/BudgetRequest.java
@@ -0,0 +1,14 @@
+package com.um.cloudfixum.cloudfixum.model;
+
+public class BudgetRequest {
+
+    private String user_email;
+
+    private String image_url_encoded;
+
+    private String description;
+
+    private String location;
+
+    private Long minor_job;
+}
