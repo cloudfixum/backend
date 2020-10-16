77c014da4ec7f67611a4685acdb8ed88750c8ce0
s.martinez <santiskatersan@gmail.com>
2020-10-16 17:27:12 -0300

    @NotNull implemented on BudgetRequest

--- src/main/java/com/um/cloudfixum/cloudfixum/model/BudgetRequest.java
+++ src/main/java/com/um/cloudfixum/cloudfixum/model/BudgetRequest.java
@@ -1,22 +1,27 @@
 package com.um.cloudfixum.cloudfixum.model;
 
+import com.um.cloudfixum.cloudfixum.common.Constant;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
 @Getter
 @Setter
 
 public class BudgetRequest implements Serializable {
-
+    @NotEmpty(message = Constant.EMAIL_NEEDED)
     private String userEmail;
 
     private String imageHash;
 
+    @NotEmpty(message = Constant.PROVIDE_DESCRIPTION)
     private String description;
 
+    @NotEmpty(message = Constant.LOCATION_NEEDED)
     private String location;
+
 
     private Long minorJobId;
 }
